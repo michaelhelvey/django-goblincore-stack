97e1818763d8cd4d25023878191ae3709d7afbb1
Michael Helvey <michael.helvey1@gmail.com>
2023-04-01 16:14:19 -0500

    Fix placeholders for create account form

--- app/views/accounts/signup.py
+++ app/views/accounts/signup.py
@@ -1,10 +1,17 @@
 from django import forms
-from django.utils.translation import gettext_lazy as _
 
 
 class CreateAccountForm(forms.Form):
-    first_name = forms.CharField(label=_("First Name"), max_length=100)
-    last_name = forms.CharField(label=_("Last Name"), max_length=100)
+    first_name = forms.CharField(
+        label="First Name",
+        max_length=100,
+        widget=forms.widgets.TextInput(attrs={"placeholder": "First Name"}),
+    )
+    last_name = forms.CharField(
+        label="Last Name",
+        max_length=100,
+        widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name"}),
+    )
 
     def signup(self, request, user):
         # Django all-auth will have already cleaned the form and returned any
